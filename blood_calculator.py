acac57174daebc9284627d58c6a3c3cd2819bdb1
Sara Min <saramin2019@gmail.com>
2022-01-12 16:35:37 -0500

    Analyze HDL test result for appropriate classication and developed HDL_driver to implement HDL workflow"

--- blood_calculator.py
+++ blood_calculator.py
@@ -10,6 +10,21 @@ def interface():
 
     return
     
+def accept_input(test_name):
+    entry = input ("Enter the {} test result:".format(test_name))
+    return int(entry)
 
+def check_HDL(HDL_value):
+    if HDL_value >= 60:
+        answer = "Normal"
+    elif 60 > HDL_value >= 40:
+        answer = "Borderline Low"
+    else:
+        answer = "Low"
+    return answer
+
+def HDL_driver():
+    HDL_value = accept_input("HDL")
+    classification = check_HDL(HDL_value)
 
 interface()
