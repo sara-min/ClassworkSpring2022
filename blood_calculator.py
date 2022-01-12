cd7ef6a18c9a95c6b4f1e8b2bfadcaf6ff0097db
Sara Min <saramin2019@gmail.com>
2022-01-12 16:50:16 -0500

    fixed typo: changed HDL to LDL

--- blood_calculator.py
+++ blood_calculator.py
@@ -4,7 +4,7 @@ def interface():
     while keep_running:
         print("Options: ")
         print("1-HDL")
-        print("2-HDL")
+        print("2-LDL")
         print("9-Quit")
         choice = input("Enter your choice: ")
         if choice == 9:
